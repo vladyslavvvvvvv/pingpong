f274066a32ddd4135b02ad7a7c6774efae8a2ae0
vladyslavvvvvvv <zebrochka228333@gmail.com>
2023-09-17 14:26:22 +0300

    add sprites and moves

--- main.py
+++ main.py
@@ -16,6 +16,11 @@ pygame.display.set_caption("Гра Пінг Понг,Автор:Владисла
 #    (width,height)
 #)
 
+background_color = (253, 253, 3)
+
+
+
+
 class GameSprite(pygame.sprite.Sprite):
     def __init__(self, image, x,y, speed, size):
         super().__init__()
@@ -37,10 +42,28 @@ class GameSprite(pygame.sprite.Sprite):
 
 class Player(GameSprite):
     def update_r(self):
-        ...
-
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_UP]:
+            self.rect.y -= self.speed
+        if keys[pygame.K_DOWN]:
+            self.rect.y += self.speed
     def update_l(self):
-        ...
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_w]:
+            self.rect.y -= self.speed
+        if keys[pygame.K_s]:
+            self.rect.y += self.speed
+
+ball = GameSprite("ball.png", width/2, height/2, 0, (50,50))
+
+racket_1 = Player("racket.png", 10, height/2-50, 7, (50,200))
+racket_2 = Player("racket.png", width-60, height/2-50, 7, (50,200))
+
+score_1 = 0
+score_2 = 0
+
+speed_x = 5
+speed_y = 5
 
 game_over = False
 finish = False
@@ -50,8 +73,34 @@ while not game_over:
         if e.type == pygame.QUIT:
                 game_over = True
 
+    window.fill(background_color)
 
+    ball.rect.x += speed_x
+    ball.rect.y += speed_y
 
+    if ball.rect.y > height-50 or ball.rect.y < 0:
+        speed_y = speed_y * (-1)
+
+    if pygame.sprite.collide_rect(ball, racket_1) or pygame.sprite.collide_rect(ball, racket_2):
+       speed_x *= -1
+       
+    if ball.rect.x > width-50:
+       score_1 += 1
+       ball.rect.x = width/2
+       ball.rect.y = height/2
+
+    if ball.rect.x < 0:
+       score_2 += 1
+
+    ball.reset()
+    ball.rect.x = width/2
+    ball.rect.y = height/2
+
+    racket_1.reset()
+    racket_2.reset()
+
+    racket_1.update_l()
+    racket_2.update_r()
 
     pygame.display.update()
     clock.tick(FPS)
